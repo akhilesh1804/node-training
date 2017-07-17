2252be56b202bf9755b8d679bee9cce2006c63c8
akhilesh1804 <agoutam@grepruby.com>
2017-07-17 11:42:52 +0530

    Assignment 1 : ES6 used , Final

--- Assignment1/unflattenMethod.js
+++ Assignment1/unflattenMethod.js
@@ -2,17 +2,19 @@
 function unflatten(flatObject) {
   const unflatObject = {};
   for (let key in flatObject) {
-    var array = key.split(".");
+    let array = key.split(".");
     myMethod(unflatObject, array, flatObject[key]);
-  }
-  
+  } 
   return unflatObject;
 }
 function myMethod (unflatObject, array, value) {
-  for (let index = 0; index < array.length-1; index++) {
-    if(!(array[index] in unflatObject))
-      /\d/g.test(array[index+1]) ? unflatObject[array[index]] = [] : unflatObject[array[index]] = {};
+  for (let index = 0; index < array.length - 1; index++) {
+    if (!(array[index] in unflatObject)) {
+      /\d/g.test(array[index + 1]) 
+      ? unflatObject[array[index]] = []
+      : unflatObject[array[index]] = {};
+    }
       unflatObject = unflatObject[array[index]];
   }
-  unflatObject[array[array.length-1]] = value;
+  unflatObject[array[array.length - 1]] = value;
 }
