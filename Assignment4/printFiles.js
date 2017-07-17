25b989663b03e3f7375c9e66d4e15748f533e274
akhilesh1804 <agoutam@grepruby.com>
2017-07-17 15:11:28 +0530

    Assignment4 : Used arrow function and minor changes

--- Assignment4/printFiles.js
+++ Assignment4/printFiles.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
-fs.readFile('File1.txt', 'utf8', function (err, data) {
+fs.readFile('File1.txt', 'utf8', (err, data) => {
   console.log("============");
   console.log(data);
-  fs.appendFile('File2.txt', data, 'utf8', function (err) {
-    fs.readFile('File2.txt', 'utf8', function (err, content) {
+  fs.appendFile('File2.txt', data, 'utf8', (err) => {
+    fs.readFile('File2.txt', 'utf8',  (err, content) {
       console.log("============");
       console.log(content);
-      fs.writeFile('File3.txt', content, 'utf8', function (err) {
-        fs.readFile('File3.txt', 'utf8', function (err, data) {
+      fs.writeFile('File3.txt', content, 'utf8', (err) => {
+        fs.readFile('File3.txt', 'utf8',  (err, data) {
           console.log("============");
           console.log(data);
           console.log("============");
         });
       });
-    })  
+    });  
   });
 });
